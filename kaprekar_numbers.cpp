872bab3ddfc3819752086d471805b52ba01cd766
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2020-10-12 12:27:56 +0530

    added comments

--- kaprekar_numbers.cpp
+++ kaprekar_numbers.cpp
@@ -4,33 +4,46 @@ using namespace std;
 
 void kaprekarNumbers(int p, int q) 
 {
+    // Storing the range in long long to prevent overflow
     long long first = p;
     long long last = q;
+
+    //flag to detect if there are any kaprekar numbers 
     int flag = 0;
+
+    //checking all numbers between p and q
     for(long long i = first; i<=last; i++)
     {
+        //converting the number to string, and then finding the no of digits, d, of the number i
         string str_i = to_string(i);
         int d = str_i.length();
 
+        // 10^d
         long long div = 1;
-
         for(int k=0; k<d; k++)
         {
-            div = div *  10;
+            div = div * 10;
         }
 
+        //square of the number
         long long square = i*i;
-        long long left = square/div;
+
+        //right has d digits
+        long long left = square / div;
         long long right = square % div;
+
+        //if its a kaprekar number print
         if(left + right == i)
         {
-            cout << i << " ";
             flag = 1;
+            cout << i << " ";
         }
-    }
-    if(flag == 0)
-    {
-        cout << "INVALID RANGE";
+
+        // if no kaprekar numbers
+        if(flag == 0)
+        {
+            cout << "INVALID RANGE";
+        }
     }
 }
 
