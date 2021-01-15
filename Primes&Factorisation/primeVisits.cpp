5df4f1957cc830290479c54a402e4af917359526
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-01-15 13:33:41 +0530

    15/01/2021

---
+++ Primes&Factorisation/primeVisits.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void primeSeive(int *p)
+{
+
+    p[0] = 0;
+    p[1] = 0;
+    p[2] = 1;
+
+    //mark all odds as prime i.e get all evens out
+    for (int i = 3; i < 1000000; i += 2)
+    {
+        p[i] = 1;
+    }
+
+    //Seive
+    for (long long i = 3; i < 1000000; i += 2)
+    {
+        //if the current number is prime
+        if (p[i] == 1)
+        {
+            for (long long j = i * i; j < 1000000; j += i)
+            {
+                //mark all the multiples of prime p[i] as not prime
+                p[j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int p[1000000] = {0};
+
+    primeSeive(p);
+
+    int a, b;
+    cin >> a >> b;
+
+    int cumulativeSum[b + 1];
+
+    cumulativeSum[0] = 0;
+
+    for (int i = 1; i <= b; i++)
+    {
+        cumulativeSum[i] = cumulativeSum[i - 1] + p[i];
+    }
+
+    cout << cumulativeSum[b] - cumulativeSum[a - 1];
+}
